e4291de2f10d935298bf9bcbfbf345927c1a01c1
T806809 <tahiyaakter94@gmail.com>
2026-07-20 15:25:07 +0600

    Setup Firebase configuration

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgmqu9Ihc4KyDlhQpMCGZwEQDm_4vLsCo",
+  authDomain: "careerpilot-ai-b810d.firebaseapp.com",
+  projectId: "careerpilot-ai-b810d",
+  storageBucket: "careerpilot-ai-b810d.firebasestorage.app",
+  messagingSenderId: "943777100022",
+  appId: "1:943777100022:web:6a118f1152df5d1df37cd7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
